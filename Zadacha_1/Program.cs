36f4ed4322edc69b860a6906db9731cabbea6291
olesybabaeva <olesybabaeva@gmail.com>
2023-10-27 20:00:42 -0700

    Задача 1 решена

--- Zadacha_1/Program.cs
+++ Zadacha_1/Program.cs
@@ -4,3 +4,19 @@
 // 3,5 -> 243 (3^5)
 // 2,4 -> 16 (2^4)
 
+Console.Write("Введите число А: ");
+int number = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число B: ");
+int pow = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine($"{number} в степени {pow} = {GetPow(pow)}");
+
+int GetPow (int B)
+{
+    int result = 0;
+    for (int i = 1; i <= B; i++)
+    {
+       result = Convert.ToInt32(Math.Pow(number, i));
+    }
+    return result;
+}
